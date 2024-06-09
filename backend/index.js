7fe564d2ffc81a59f8454fc841303469210cc633
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-09 16:56:59 +0400

    fix erorr

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const cors = require("cors");
+const allowCors = require("./config/cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const router = require("./routes");
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-  })
-);
+app.use(allowCors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
