b6fc788081186c2ca3787bd338f59550d582f9b9
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-09 22:09:12 +0400

    fix erorr

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,4 @@
-const backendDomin = process.env.REACT_APP_BACKEND_URL;
+const backendDomin = "https://full-stack-e-commerce-rho.vercel.app/";
 
 const SummaryApi = {
   signUp: {
--- frontend/src/component/CategoryList.js
+++ frontend/src/component/CategoryList.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import SummaryApi from "../common";
 import { Link } from "react-router-dom";
-import productCategory from "../helpers/productCategory";
 
 const CategoryList = () => {
   const [categoryProduct, setCategoryProduct] = useState([]);
@@ -12,22 +11,23 @@ const CategoryList = () => {
   const fetchCategoryProduct = async () => {
     setLoading(true);
     const response = await fetch(SummaryApi.categoryProduct.url);
-    const DataResponse = await response.json();
+    const dataResponse = await response.json();
     setLoading(false);
-    setCategoryProduct(DataResponse.data);
+    setCategoryProduct(dataResponse.data);
   };
 
   useEffect(() => {
     fetchCategoryProduct();
   }, []);
+
   return (
-    <div className=" mx-auto p-4  bg-[rgb(18,35,45)] border-y-2 text-white ">
-      <div className="flex items-center justify-between gap-4 overflow-scroll scrollbar-none ">
+    <div className="container mx-auto p-4">
+      <div className="flex items-center gap-4 justify-between overflow-scroll scrollbar-none">
         {loading
           ? categoryLoading.map((el, index) => {
               return (
                 <div
-                  className="h-16 -w-16 md:w-20 md:h-20 rounded-full overflow-hidden bg-slate-200 animate-pulse "
+                  className="h-16 w-16 md:w-20 md:h-20 rounded-full overflow-hidden bg-slate-200 animate-pulse"
                   key={"categoryLoading" + index}
                 ></div>
               );
@@ -35,17 +35,18 @@ const CategoryList = () => {
           : categoryProduct.map((product, index) => {
               return (
                 <Link
-                  to={"/product-category/?category=" + product?.category}
-                  className=" cursor-pointer hover:text-green-600 "
+                  to={"/product-category?category=" + product?.category}
+                  className="cursor-pointer"
+                  key={product?.category}
                 >
-                  <div className="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden bg-slate-200  p-3 flex justify-center items-center">
+                  <div className="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-4 bg-slate-200 flex items-center justify-center">
                     <img
                       src={product?.productImage[0]}
                       alt={product?.category}
-                      className="h-full object-scale-down mix-blend-multiply hover:scale-125 transition-all "
+                      className="h-full object-scale-down mix-blend-multiply hover:scale-125 transition-all"
                     />
                   </div>
-                  <p className="text-center text-sm md:text-base capitalize my-1 ">
+                  <p className="text-center text-sm md:text-base capitalize">
                     {product?.category}
                   </p>
                 </Link>
