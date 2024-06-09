6b4f5fefe9bc9370554e157cb75897a412a29ac9
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-09 17:09:36 +0400

    fix erorr

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const router = require("./routes");
 const app = express();
 app.use(
   cors({
+    "Access-Control-Allow-Origin": process.env.FRONTEND_URL,
     origin:
       "https://full-stack-e-commerce-rho.vercel.app" ||
       process.env.FRONTEND_URL,
