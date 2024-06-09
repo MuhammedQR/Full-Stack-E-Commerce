30dcd8cd2caea60b5c58ae37b8e9c803359087ff
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-09 23:01:54 +0400

    fix erorr

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,6 @@ const router = require("./routes");
 
 const app = express();
 app.use(
-  "*",
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
-app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
