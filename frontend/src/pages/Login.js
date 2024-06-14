43290e5f76fc9a3a02d3310b6440eefcaaf4c8d8
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-14 14:30:34 +0400

    add

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -59,10 +59,7 @@ const Login = () => {
           content=" Log In to Mo Store Now "
         />
         {/* Add a relevant description */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
+     
         {/* Ensure proper mobile responsiveness */}
         {/* Add additional meta tags as needed */}
       </Helmet>
